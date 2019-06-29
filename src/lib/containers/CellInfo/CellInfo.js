9c25eba3bf75e2d4097d5c3ac56f64619b9e44bf
Kyzyl-ool <kyzyloolk@mail.ru>
2019-06-29 14:08:35 +0300

    Applying Typography

--- src/lib/containers/CellInfo/CellInfo.js
+++ src/lib/containers/CellInfo/CellInfo.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import styles from './CellInfo.module.scss';
 import {currency, energy} from "../../constatnts/names";
+import Typography from '@material-ui/core/Typography';
 
 
 function CellInfo({name, amount, money}) {
     return <div className={styles.CellInfo}>
-        {name && <b>{name}</b>}
-        <br/>
-        {amount && `${amount} ${energy}`}
-        <br/>
-        {money && `${money} ${currency}`}
+        {name && <Typography color='textPrimary' variant='body1'><b>{name}</b></Typography>}
+        {amount && <Typography color='secondary' variant='body1'>{amount} {energy}</Typography>}
+        {money && <Typography color='textSecondary' variant='body1'>{money} {currency}</Typography>}
     </div>
 }
 
