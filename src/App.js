4a632ce02b614a99a84a0a6fa517a9999cb75bf6
Kyzyl-ool <kyzyloolk@mail.ru>
2019-06-29 20:11:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import MainPage from "./lib/pages/MainPage/MainPage";
 import {createMuiTheme} from "@material-ui/core";
 import {responsiveFontSizes} from "@material-ui/core/styles";
 import { ThemeProvider } from '@material-ui/styles';
-
+import './App.scss';
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
-
-import './App.scss';
 
 function App() {
   return (
