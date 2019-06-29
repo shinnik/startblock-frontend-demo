635da9b8f2d4e05376aad10179bf38bced0a55eb
Kyzyl-ool <kyzyloolk@mail.ru>
2019-06-29 19:30:50 +0300

    Responsive?

--- src/lib/containers/Delta/Delta.js
+++ src/lib/containers/Delta/Delta.js
@@ -5,15 +5,16 @@ import Typography from '@material-ui/core/Typography/index';
 import {currency} from "../../constatnts/names";
 import Button from '@material-ui/core/Button/index';
 import Box from '@material-ui/core/Box';
+import {makeStyles} from "@material-ui/core";
 
 
 function Delta({name, type, money}) {
     return <Box className={styles.Box}>
         <Paper className={styles.Delta}>
-            <Typography className={styles.Item1} variant='h3'> {name} </Typography>
-            <Typography className={styles.Item5} variant='body1'> {type} </Typography>
-            <Typography className={styles.Item2} variant='h3'> {money} </Typography>
-            <Typography className={styles.Item6} variant='body1'> {currency} </Typography>
+            <span className={styles.Item1} variant='h3'> {name} </span>
+            <span className={styles.Item5} variant='body1'> {type} </span>
+            <span className={styles.Item2} variant='h3'> {money} </span>
+            <span className={styles.Item6} variant='body1'> {currency} </span>
             <Button className={styles.Item3} variant='contained' color='default'>Пополнить</Button>
             <Button className={styles.Item4} variant='outlined' color='default' >Снять</Button>
         </Paper>
