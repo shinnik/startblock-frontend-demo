01a3acb4daf12efc1ef9895d854a232be5d6c28b
Kyzyl-ool <kyzyloolk@mail.ru>
2019-06-29 12:49:39 +0300

    Outline data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,39 @@ import FlexHorizontal from "./lib/wrappers/flex-horizontal/Flex-Horizontal";
 import CellInfo from "./lib/containers/CellInfo/CellInfo";
 import MultiArrow from "./lib/containers/MultiArrow/MultiArrow";
 
+const arr = [true, true, false, false, true];
+const data = [
+    {
+        name: 'Alpha',
+        amount: 52,
+        money: 5
+    },
+    {
+        name: 'Beta',
+        amount: 2,
+        money: 1
+    },
+    {
+        name: 'Gamma',
+        amount: 512,
+        money: 59
+    },
+    {
+        name: 'Gamma',
+        amount: 512,
+        money: 59
+    },
+    {
+        name: 'Gamma',
+        amount: 512,
+        money: 59
+    },
+];
 
 function App() {
   return (
     <div className="App">
-       <MultiArrow/>
+       <MultiArrow arr={arr} data={data}/>
     </div>
   );
 }
--- src/lib/containers/MultiArrow/MultiArrow.js
+++ src/lib/containers/MultiArrow/MultiArrow.js
@@ -4,39 +4,8 @@ import MediumArrow from "../../components/BranchArrows/MediumArrow/MediumArrow";
 import CellInfo from "../CellInfo/CellInfo";
 import styles from './MultiArrow.module.scss';
 
-
-const arr = [true, true, false, false, true];
-const data = [
-    {
-        name: 'Alpha',
-        amount: 52,
-        money: 5
-    },
-    {
-        name: 'Beta',
-        amount: 2,
-        money: 1
-    },
-    {
-        name: 'Gamma',
-        amount: 512,
-        money: 59
-    },
-    {
-        name: 'Gamma',
-        amount: 512,
-        money: 59
-    },
-    {
-        name: 'Gamma',
-        amount: 512,
-        money: 59
-    },
-];
-
-function MultiArrow() {
+function MultiArrow({arr, data}) {
     return <div>
-        asjdfgkjasdgfjasgdfasgdfhjsgdfagsfkhasgfkjashgfhjasgdfkhasdgfkjhasdgfhj
         <FlexHorizontal>
             <div>
                 {
