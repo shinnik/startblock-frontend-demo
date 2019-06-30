9cac04c661fb2593906a8fd76b18bd604201aac2
Kyzyl-ool <kyzyloolk@mail.ru>
2019-06-30 17:16:35 +0300

    More good css and removing excess borders

--- src/lib/containers/ArrowAndInfo/ArrowAndInfo.js
+++ src/lib/containers/ArrowAndInfo/ArrowAndInfo.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './ArrowAndInfo.module.scss';
 import Typography from '@material-ui/core/Typography';
 import {currency, energy} from "../../constatnts/names";
-
+import numberSeparator from "../../../utils/numberSeparator";
 
 
 function ArrowAndInfo({direction, amount, money}) {
     return <div className={styles.ArrowAndInfo}>
         <div style={{paddingLeft: '20px', paddingTop: '20px'}}>
-            <Typography variant='h5' color='secondary'>{amount && `${amount} ${energy}` || <br/>}</Typography>
+            <Typography variant='h5' color='secondary'>{amount && `${numberSeparator(amount)} ${energy}` || <br/>}</Typography>
             <Typography variant='h5' color="textSecondary" >{money && `${money} ${currency}` || <br/>}</Typography>
         </div>
         <div className={direction ? styles.TriangleUp : styles.TriangleDown} />
