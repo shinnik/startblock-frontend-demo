8de215574793e65face00197b387068e111ed4e7
Kyzyl-ool <kyzyloolk@mail.ru>
2019-07-08 10:12:55 +0300

    Arrows fix

---
+++ src/lib/components/BranchArrows/MediumArrow/ExtraBorder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './MediumArrow.module.scss';
+
+/**
+ * @return {null}
+ */
+function ExtraBorder({last, first, active, ggrey}) {
+    if (first)
+        return <div className={styles.FirstExtraBorder} />;
+    if (!active && !ggrey)
+        return  <div className={styles.ExtraBorder} />;
+    return null;
+}
+
+export default ExtraBorder;
--- src/lib/components/BranchArrows/MediumArrow/MediumArrow.js
+++ src/lib/components/BranchArrows/MediumArrow/MediumArrow.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Triangle from "./Triangle/Triangle";
 import Border from "./Border/Border";
-import styles from './MediumArrow.module.scss';
+import ExtraBorder from "./ExtraBorder";
 
 
 function MediumArrow({direction, last, first, active, ggrey}) {
     return <Border ggrey={ggrey} last={last} first={first} active={active}>
         <Triangle direction={direction} first={first} active={active} />
-        {!ggrey ? (first ? <div className={styles.FirstExtraBorder}/> : (!last ? <div className={styles.ExtraBorder} /> : null)) : null}
+        <ExtraBorder active={active} first={first} last={last} ggrey={ggrey} />
     </Border>;
 }
 
