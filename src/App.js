fe66cdfdb301d1f5eeffee4f3e7a409c3078bc28
Nikita Izrailev <izrailev@phystech.edu>
2019-07-09 01:55:20 +0300

    added lazy loading for routes and suspense component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { ThemeProvider } from '@material-ui/styles';
-import MainPage from "./lib/pages/MainPage/MainPage";
-import SettingsPage from "./lib/pages/SettingsPage/SettingsPage";
 import { Layout } from "./lib/components/Layout/Layout";
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import theme from './styles/theme'
 import './App.scss';
 import * as mockData from './store/mockData/mockData';
+import {Loader} from "./lib/components/Loader/Loader";
+
+const MainPage = React.lazy(() => import("./lib/pages/MainPage/MainPage"));
+const SettingsPage = React.lazy(() => import("./lib/pages/SettingsPage/SettingsPage"));
 
 
 function App() {
@@ -14,15 +16,17 @@ function App() {
         <div className="App">
             <ThemeProvider theme={theme}>
                 <Router basename={process.env.PUBLIC_URL}>
-                    <Layout>
-                        <Switch>
-                            <Route path='/current' component={() => <MainPage profile={mockData.profile} data={mockData.data} multidata2={mockData.multidata2} multidata={mockData.multidata} flag="current" />} />
-                            <Route path='/month' component={() => <MainPage profile={mockData.profile} data={mockData.data} multidata={mockData.multidata} multidata2={mockData.multidata2} flag="month" />} />
-                            <Route path='/settings' component={() => <SettingsPage />} />
-                            <Redirect to="/current"/>
-                        </Switch>
-                    </Layout>
-                </Router>
+                        <Layout>
+                            <Suspense fallback={<Loader/>}>
+                                <Switch>
+                                    <Route path='/current' component={() => <MainPage profile={mockData.profile} data={mockData.data} multidata2={mockData.multidata2} multidata={mockData.multidata} flag="current" />} />
+                                    <Route path='/month' component={() => <MainPage profile={mockData.profile} data={mockData.data} multidata={mockData.multidata} multidata2={mockData.multidata2} flag="month" />} />
+                                    <Route path='/settings' component={() => <SettingsPage />} />
+                                    <Redirect to="/current"/>
+                                </Switch>
+                            </Suspense>
+                        </Layout>
+                    </Router>
             </ThemeProvider>
         </div>
     );
