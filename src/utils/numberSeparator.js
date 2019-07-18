7a3d0ff549653007ac2fd10ae3f378445666e8b6
Kyzyl-ool <kyzyloolk@mail.ru>
2019-07-18 12:41:04 +0300

    Update numberSeparator.js

--- src/utils/numberSeparator.js
+++ src/utils/numberSeparator.js
@@ -4,11 +4,13 @@ export default function numberFormat(num) {
     if (!isFinite(num)) {
         return num;
     }
-
-    var parts = num.toString().split('.');
-
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-
-
-    return parts.join('.');
+    const parts = num.toString().split('.');
+    const p = parts[0].split(/\B(?=(\d{3})+(?!\d))/g);
+    const q = p.slice(0, -1);
+    if (q.length < 2)
+        return num;
+    return q.map((value, index) => <span key={index}>
+        {value}
+        {index < q.length-1 && <span style={{fontSize: '8px'}}> </span>}
+    </span>);
 }
