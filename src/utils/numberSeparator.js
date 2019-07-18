317a76891671d0441f6a60f336c3736425c6d519
Kyzyl-ool <kyzyloolk@mail.ru>
2019-07-18 12:22:47 +0300

    Update numberSeparator.js

--- src/utils/numberSeparator.js
+++ src/utils/numberSeparator.js
@@ -7,7 +7,7 @@ export default function numberFormat(num) {
 
     var parts = num.toString().split('.');
 
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 
 
     return parts.join('.');
