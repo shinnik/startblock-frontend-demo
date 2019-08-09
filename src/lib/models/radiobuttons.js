96ac82c2d99aa68d59264850500b3b7788c0e07e
Nikita Izrailev <izrailev@phystech.edu>
2019-08-09 10:59:43 +0300

    added third radiobutton

--- src/lib/models/radiobuttons.js
+++ src/lib/models/radiobuttons.js
@@ -11,13 +11,23 @@ export const p2pradios = [{
         </div>
     ),
     color: 'white'
-},
+    },
     {
         value: '2',
         label: (
-            <div style={{display: 'flex', flexDirection: 'column', color: 'white', marginBottom: '10px', marginTop: '20px'}}>
+            <div style={{display: 'flex', flexDirection: 'column', color: 'white', marginBottom: '10px', marginTop: '10px'}}>
                 <Typography key='1' variant="body1">{config.settingsPage.services.service2.radios[1].label}</Typography>
                 <Typography key='2' style={{ opacity: 0.54 }} variant="body2">{config.settingsPage.services.service2.radios[1].caption}</Typography>
+            </div>
+        ),
+        color: 'white'
+    },
+    {
+        value: '3',
+        label: (
+            <div style={{display: 'flex', flexDirection: 'column', color: 'white'}}>
+                <Typography key='1' variant="body1">{config.settingsPage.services.service2.radios[2].label}</Typography>
+                <Typography key='2' style={{ opacity: 0.54 }} variant="body2">{config.settingsPage.services.service2.radios[2].caption}</Typography>
             </div>
         ),
         color: 'white'
