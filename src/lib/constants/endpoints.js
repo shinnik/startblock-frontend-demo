36f3d667c590134ea81d7d520bc0344bd2ff7cae
Kyzyl-ool <kyzyloolk@mail.ru>
2020-01-22 21:44:05 +0300

    Update endpoints.js

--- src/lib/constants/endpoints.js
+++ src/lib/constants/endpoints.js
@@ -1,10 +1,10 @@
-const serverName = '34.65.103.172';
+const serverName = '192.168.0.14';
 // const ethAddress = '0x5fc4558c67bf6886ee453aec35af7ea8021eab7a'; //2
-const ethAddress = '0x512B41D07C70087A1BaAC4d1A49D55B051B05f4a'; //1
+const ethAddress = '0xaa3a672cfb69796e5b61ac63d31dd01e614a3d3c'; //1
 // const ethAddress = '0xD1A513c48b7f370c1e2eDE1F7673D1819ed00FD2'; //3
 // const ethAddress = '0x44ac47379573F997098d02714D652abc28629c44'; //4
 // const serverName = '192.168.0.39';
-const port = 8888;
+const port = 8881;
 // const uri = 'api';
 // const port = undefined;
 const uri = null;
